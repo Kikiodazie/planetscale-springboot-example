8ef04ad50fb7b591e9fb78788bf966f86d6ceaf0
Kikiodazie <kiki.odazie@yahoo.com>
2022-01-16 14:29:17 +0100

    Initial commit

---
+++ src/main/java/com/odazie/planetscalespringbootexample/PlanetscaleSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.odazie.planetscalespringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetscaleSpringbootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanetscaleSpringbootExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odazie/planetscalespringbootexample/PlanetscaleSpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odazie.planetscalespringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetscaleSpringbootExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
